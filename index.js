eeb9b9cd3e4dac7e2f15f938833444cee92bb16a
fredy kardian <fred.kard@gmail.com>
2020-12-02 14:49:16 +0700

    commit branch-99 with selamat sore

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const app = express()
 
 app.get('/',(reg, res)=>{
     console.log('A request happened\'/\'')
-    res.send('Hello World, from express')
+    res.send('Selamat sore, from express')
 })
 
 app.get('/udo',(reg, res)=>{
     console.log('A request happened in route \'/udo\'')
-    res.send('Hello World, from udo')
+    res.send('Selamat sore, from udo')
 })
 
 app.listen(port,()=> console.log(`Hello world app listening on port ${port}!`))
